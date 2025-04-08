1fbcdd0ceb7a9f307ee6cddf211a14ece5f76918
William Faunce <fonz.dev@pm.me>
2025-04-08 19:34:02 -0400

    add tracing to client executable

--- client/src/bin/client.rs
+++ client/src/bin/client.rs
@@ -1,11 +1,19 @@
 use anyhow::Error;
 use signaling::client::Client;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let mut client = Client::new().expect("Failed to create client");
 
     client.make_whip_request().await?;
+
+    info!("Is the RTC alive? {:?}", client.rtc.is_alive());
 
     Ok(())
 }
