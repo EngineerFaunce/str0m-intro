a11434c90bfa720d7027db28de813f8ff312ca57
William Faunce <fonz.dev@pm.me>
2025-10-23 09:22:28 -0400

    temp: remove http redirect server
    Not able to wrap my head around how to get this
    working within structured concurrency yet, so shelving it.

--- signaling/src/bin/server.rs
+++ signaling/src/bin/server.rs
@@ -1,18 +1,13 @@
 use axum::extract::State;
-use axum::handler::HandlerWithoutStateExt;
-use axum::http::uri::Authority;
-use axum::http::Uri;
-use axum::response::{Redirect, Response};
+use axum::response::Response;
 use axum::routing::post;
 use axum::Router;
-use axum::{BoxError, Json};
-use axum_extra::extract::Host;
+use axum::Json;
 use axum_server::tls_rustls::RustlsConfig;
 use core::panic;
-use reqwest::StatusCode;
 use signaling::client::Client;
 use std::collections::HashMap;
-use std::future::Future;
+use std::future::IntoFuture;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::time::Duration;
@@ -25,7 +20,6 @@ use uuid::Uuid;
 
 #[derive(Clone, Copy)]
 struct Ports {
-    http: u16,
     https: u16,
 }
 
@@ -41,17 +35,8 @@ async fn main() {
         .init();
 
     let ports = Ports {
-        http: 7878,
         https: 3000,
     };
-
-    // Create a handle for our TLS server so the shutdown signal can all shutdown
-    let handle = axum_server::Handle::new();
-    // save the future for easy shutting down of redirect server
-    let shutdown_future = shutdown_signal(handle.clone());
-
-    // optional: spawn a second server to redirect http requests to this server
-    tokio::spawn(redirect_http_to_https(ports, shutdown_future));
 
     let (tx, rx): (Sender<Client>, mpsc::Receiver<Client>) = mpsc::channel(10);
     let state = AppState {
@@ -174,50 +159,3 @@ async fn shutdown_signal(handle: axum_server::Handle) {
                                                              // to force shutdown
 }
 
-async fn redirect_http_to_https<F>(ports: Ports, signal: F)
-where
-    F: Future<Output = ()> + Send + 'static,
-{
-    fn make_https(host: &str, uri: Uri, https_port: u16) -> Result<Uri, BoxError> {
-        let mut parts = uri.into_parts();
-
-        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
-
-        if parts.path_and_query.is_none() {
-            parts.path_and_query = Some("/".parse().unwrap());
-        }
-
-        let authority: Authority = host.parse()?;
-        let bare_host = match authority.port() {
-            Some(port_struct) => authority
-                .as_str()
-                .strip_suffix(port_struct.as_str())
-                .unwrap()
-                .strip_suffix(':')
-                .unwrap(), // if authority.port() is Some(port) then we can be sure authority ends with :{port}
-            None => authority.as_str(),
-        };
-
-        parts.authority = Some(format!("{bare_host}:{https_port}").parse()?);
-
-        Ok(Uri::from_parts(parts)?)
-    }
-
-    let redirect = move |Host(host): Host, uri: Uri| async move {
-        match make_https(&host, uri, ports.https) {
-            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
-            Err(error) => {
-                tracing::warn!(%error, "failed to convert URI to HTTPS");
-                Err(StatusCode::BAD_REQUEST)
-            }
-        }
-    };
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], ports.http));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    tracing::debug!("listening on {addr}");
-    axum::serve(listener, redirect.into_make_service())
-        .with_graceful_shutdown(signal)
-        .await
-        .unwrap();
-}
