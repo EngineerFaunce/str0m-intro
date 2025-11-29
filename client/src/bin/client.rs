f1fcae01544daf23a2bd1862f48dfc413567e786
William Faunce <fonz.dev@pm.me>
2025-11-28 19:10:58 -0500

    client structured concurrency

--- client/src/bin/client.rs
+++ client/src/bin/client.rs
@@ -4,6 +4,7 @@ use tokio::{
     sync::mpsc::{self, Receiver, Sender},
     task::JoinSet,
 };
+use tokio_util::sync::CancellationToken;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -18,19 +19,61 @@ async fn main() -> Result<(), Error> {
 
     // * Channel for RTP packets
     let (tx, rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel(5);
-    let mut set = JoinSet::new();    
+    let token = CancellationToken::new();
+    let mut set = JoinSet::new();
 
-    set.spawn_blocking(move || media::stream_test_video(tx.clone()));
-    set.spawn(run_client_loop(client, rx));
+    set.spawn_blocking(move || media::stream_test_video(tx));
 
-    set.join_all().await;
+    let client_token = token.clone();
+    set.spawn(run_client_loop(client, rx, client_token));
+
+    let mut failure: Option<Error> = None;
+    while let Some(result) = set.join_next().await {
+        match result {
+            Ok(Ok(())) => {}
+            Ok(Err(e)) => {
+                token.cancel();
+                failure.get_or_insert(e);
+                break;
+            }
+            Err(join_error) => {
+                token.cancel();
+                failure.get_or_insert(join_error.into());
+                break;
+            }
+        }
+    }
+
+    if let Some(err) = failure {
+        while set.join_next().await.is_some() {}
+        return Err(err);
+    }
 
     Ok(())
 }
 
-async fn run_client_loop(mut client: Client, mut rx: Receiver<Vec<u8>>) -> Result<(), Error> {
+async fn run_client_loop(
+    mut client: Client,
+    mut rx: Receiver<Vec<u8>>,
+    token: CancellationToken,
+) -> Result<(), Error> {
     loop {
-        client.run().await?;
-        client.send_video(&mut rx)?;
+        tokio::select! {
+            res = client.run(token.clone()) => {
+                res?;
+            }
+            _ = token.cancelled() => break,
+        }
+
+        tokio::select! {
+            res = async {
+                client.send_video(&mut rx).map_err(Error::from)
+            } => {
+                res?;
+            }
+            _ = token.cancelled() => break,
+        }
     }
+
+    Ok(())
 }
