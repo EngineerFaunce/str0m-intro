9b1d04fb8d6cb804c1bb1a09e35f4f67d57efccb
William Faunce <fonz.dev@pm.me>
2026-01-19 11:52:39 -0500

    fix path to certs

--- rtc/src/lib.rs
+++ rtc/src/lib.rs
@@ -153,6 +153,8 @@ impl Client {
 
         // TODO: should the certificate and key be moved to a more central location?
         let temp = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("server")
             .join("self_signed_certs")
             .join("cert.pem");
         let mut file = File::open(temp).await?;
